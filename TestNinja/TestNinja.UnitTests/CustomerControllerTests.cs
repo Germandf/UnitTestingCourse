30191a050dfa1078a9f05561e7fb0bbfb2f32765
German De Francesco <germandefrancesco@hotmail.com>
2022-09-14 18:24:14 -0300

    Implement CustomerControllerTests

---
+++ TestNinja/TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests;
+
+[TestFixture]
+public class CustomerControllerTests
+{
+    private CustomerController _customerController = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _customerController = new CustomerController();
+    }
+
+    [Test]
+    public void GetCustomer_IdIsZero_ReturnNotFound()
+    {
+        var result = _customerController.GetCustomer(0);
+
+        // Exactly type
+        Assert.That(result, Is.TypeOf<NotFound>());
+
+        // Type or one of its derivatives
+        Assert.That(result, Is.InstanceOf<NotFound>());
+    }
+
+    [Test]
+    public void GetCustomer_IdIsNotZero_ReturnOk()
+    {
+        var result = _customerController.GetCustomer(1);
+
+        Assert.That(result, Is.TypeOf<Ok>());
+    }
+}
