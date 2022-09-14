2fd4e0f364bfff4c2289ea66fe3c5ac01381ced8
German De Francesco <germandefrancesco@hotmail.com>
2022-09-14 19:23:23 -0300

    Add FizzBuzzTests

---
+++ TestNinja/TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests;
+
+[TestFixture]
+public class FizzBuzzTests
+{
+    [Test]
+    [TestCase(15, "FizzBuzz", TestName = "GetOutput_InputIsDivisibleBy3And5_ReturnFizzBuzz")]
+    [TestCase(3, "Fizz", TestName = "GetOutput_InputIsDivisibleBy3Only_ReturnFizz")]
+    [TestCase(5, "Buzz", TestName = "GetOutput_InputIsDivisibleBy5Only_ReturnBuzz")]
+    [TestCase(1, "1", TestName = "GetOutput_InputIsNotDivisibleBy3Or5_ReturnTheSameNumber")]
+    public void GetOutput_WhenCalled_ReturnExpectedResult(int input, string expectedOutput)
+    {
+        var output = FizzBuzz.GetOutput(input);
+
+        Assert.AreEqual(expectedOutput, output);
+    }
+}
