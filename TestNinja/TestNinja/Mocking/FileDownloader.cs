dcf343b10a43b3e3631ecae5ab4db20bb8fc039d
German De Francesco <germandefrancesco@hotmail.com>
2022-09-17 14:20:26 -0300

    Add InstallerHelperTests

---
+++ TestNinja/TestNinja/Mocking/FileDownloader.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace TestNinja.Mocking;
+
+public interface IFileDownloader
+{
+    void DownloadFile(string url, string path);
+}
+
+public class FileDownloader : IFileDownloader
+{
+    public void DownloadFile(string url, string path)
+    {
+#pragma warning disable SYSLIB0014 // Type or member is obsolete
+        var client = new WebClient();
+#pragma warning restore SYSLIB0014 // Type or member is obsolete
+        client.DownloadFile(url, path);
+    }
+}
--- TestNinja/TestNinja/Mocking/InstallerHelper.cs
+++ TestNinja/TestNinja/Mocking/InstallerHelper.cs
@@ -4,19 +4,19 @@ namespace TestNinja.Mocking
 {
     public class InstallerHelper
     {
-        private string _setupDestinationFile;
+        private readonly IFileDownloader _fileDownloader;
+        private string _setupDestinationFile = string.Empty;
+
+        public InstallerHelper(IFileDownloader fileDownloader)
+        {
+            _fileDownloader = fileDownloader;
+        }
 
         public bool DownloadInstaller(string customerName, string installerName)
         {
-            var client = new WebClient();
             try
             {
-                client.DownloadFile(
-                    string.Format("http://example.com/{0}/{1}",
-                        customerName,
-                        installerName),
-                    _setupDestinationFile);
-
+                _fileDownloader.DownloadFile($"http://example.com/{customerName}/{installerName}", _setupDestinationFile);
                 return true;
             }
             catch (WebException)
